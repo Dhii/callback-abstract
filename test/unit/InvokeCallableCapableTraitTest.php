1cd9974b0e0a1bcfa5f4942019bffa1a3509d987
Anton Ukhanev <xedin.unknown@gmail.com>
2018-01-16 11:01:56 +0100

    Exceptions in callables must now be wrapped in a standard exception

--- test/unit/InvokeCallableCapableTraitTest.php
+++ test/unit/InvokeCallableCapableTraitTest.php
@@ -2,10 +2,12 @@
 
 namespace Dhii\Invocation\UnitTest;
 
+use Dhii\Invocation\Exception\InvocationExceptionInterface;
 use Traversable;
 use Xpmock\TestCase;
 use Dhii\Invocation\InvokeCallableCapableTrait as TestSubject;
 use InvalidArgumentException;
+use Exception as RootException;
 
 /**
  * Tests {@see TestSubject}.
@@ -31,6 +33,11 @@ class InvokeCallableCapableTraitTest extends TestCase
     public function createInstance()
     {
         $mock = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+                ->setMethods([
+                    '__',
+                    '_createInvalidArgumentException',
+                    '_createInvocationException',
+                ])
                 ->getMockForTrait();
         $mock->method('__')
                 ->will($this->returnArgument(0));
@@ -38,6 +45,12 @@ class InvokeCallableCapableTraitTest extends TestCase
                 ->will($this->returnCallback(function ($message) {
                     return new InvalidArgumentException($message);
                 }));
+        $mock->method('_createInvocationException')
+            ->will($this->returnCallback(function ($message = '', $code = 0, $inner = null, $callable = null, $args = null) {
+                $e = $this->createInvocationException($message, $code, $inner, $callable, $args);
+                return $e;
+            }));
+
         $mock->method('_normalizeArray')
             ->will($this->returnCallback(function($list) {
                 return $list instanceof Traversable
@@ -46,6 +59,49 @@ class InvokeCallableCapableTraitTest extends TestCase
             }));
 
         return $mock;
+    }
+
+    /**
+     * Creates a mock that both extends a class and implements interfaces.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $className      Name of the class for the mock to extend.
+     * @param string $interfaceNames Names of the interfaces for the mock to implement.
+     *
+     * @return object The object that extends and implements the specified class and interfaces.
+     */
+    public function mockClassAndInterfaces($className, $interfaceNames = [])
+    {
+        $paddingClassName = uniqid($className);
+        $definition = vsprintf('abstract class %1$s extends %2$s implements %3$s {}', [
+            $paddingClassName,
+            $className,
+            implode(', ', $interfaceNames),
+        ]);
+        eval($definition);
+        return $this->mock($paddingClassName);
+    }
+
+    /**
+     * Creates a new Invocation exception.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $message The error message, if any.
+     * @param int $code The error code, if any.
+     * @param null $inner The inner exception, if any.
+     * @param null $callable The callable that caused the problem, if any.
+     * @param null $args The args that the callable was invoked with, if any.
+     *
+     * @return InvocationExceptionInterface The new exception.
+     */
+    public function createInvocationException($message = '', $code = 0, $inner = null, $callable = null, $args = null)
+    {
+        return $this->mockClassAndInterfaces('Exception', ['Dhii\Invocation\Exception\InvocationExceptionInterface'])
+            ->getCallable($callable)
+            ->getArgs($args)
+            ->new($message, $code, $inner);
     }
 
     /**
@@ -101,5 +157,36 @@ class InvokeCallableCapableTraitTest extends TestCase
         $result = $_subject->_invokeCallable($callable, $args);
 
         $this->assertEquals($args, $result, 'Invocation produced a wrong result');
+    }
+
+    /**
+     * Tests whether invoking a callable fails as expected if the callable throws.
+     *
+     * @since [*next-version*]
+     */
+    public function testInvokeCallableInvocationException()
+    {
+        $args = ['Apple', 'Banana'];
+        $innerMessage = uniqid('inner-message');
+        $exception = new RootException($innerMessage);
+        $callable = function () use ($exception) {
+            throw $exception;
+        };
+
+        $subject = $this->createInstance();
+        $_subject = $this->reflect($subject);
+
+        $subject->expects($this->exactly(1))
+            ->method('_createInvocationException')
+            ->with(
+                $this->isType('string'),
+                $this->anything(),
+                $exception,
+                $callable,
+                $args
+            );
+
+        $this->setExpectedException('Dhii\Invocation\Exception\InvocationExceptionInterface');
+        $_subject->_invokeCallable($callable, $args);
     }
 }
